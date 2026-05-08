dee515b863087a02a64da882972288a987e6140b
Marcus <marcus.duggs@icloud.com>
2026-05-08 10:46:03 -0700

    fix: disable S3 ACLs (bucket policy handles public read on new buckets)

--- golf_caddie/settings.py
+++ golf_caddie/settings.py
@@ -145,7 +145,7 @@ STORAGES = {
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
 AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-2')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None          # bucket policy handles public read; ACLs disabled on new buckets
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_QUERYSTRING_AUTH = False
 
